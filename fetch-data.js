139279f7333e89ead3f927887da0180e0a5039da
Vallor23 <enosnyabinda2017@gmail.com>
2024-07-20 11:15:24 +0300

    Modified fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,28 +1,29 @@
 // Initialize the async function
 async function fetchUserData() {
     
-    const apiUrl = 'https://jsonplaceholder.typicode.com/users';    // define the api url
-    const dataContainer = document.getElementById('api-data');      //storage for api data     
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';// define the api url
+    const dataContainer = document.getElementById('api-data');//storage for api data     
 
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');   //getting data from apiUrl
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');//getting data from apiUrl
         const users = await response.json();
-        dataContainer.innerHTML = '';   //clear the loading message
+        dataContainer.innerHTML = '';//clear the loading message
         // create ul element
         const userList = document.createElement('ul');
-        users.forEach(user=> {      //loop through users to create li element
+        users.forEach((user)=> {//loop through users to create li element
             //create and append the li element
-            document.createElement('li').textContent=user.name;
-            userList.appendChild('li');
+            const li = document.createElement('li')
+            li.textContent=user.name;// Set the textContent of the <li> to the user's name
+            userList.appendChild(li);
         });
 
-        // Appen ul element/userlist to the data container for display
+        // Append ul element/userlist to the data container for display
         dataContainer.appendChild(userList);
 
     } catch (error) {
-        dataContainer.innerHTML = '';       //clear the loading message
-        dataContainer.textContent = 'Failed to load user data.';        //indicates an eror occured during fetching data        
+        dataContainer.innerHTML = '';//clear the loading message
+        dataContainer.textContent = 'Failed to load user data.';//indicates an eror occured during fetching data        
     }
 }
 
-document.addEventListener('DOMContentLoaded', fetchUserData);     //Invoke fetchUserData
+document.addEventListener('DOMContentLoaded', fetchUserData);//Invoke fetchUserData
